ba9353b429ec220314e7dc027cc9e306a6f19a65
Andy Sukanto Kan,Andy A <a.kan@student.fontys.nl>
2018-08-29 20:37:45 +0200

    Tasks completed

---
+++ src/app/tasks.ts
@@ -0,0 +1,10 @@
+export class task{
+    id:number;
+    name:string;
+    priority:string;
+};
+export const tasks:task[]=[
+    {id:1,name:'sending biljo to hell',priority:'Number one'},
+    {id:2,name:'Finish this task',priority:'Kinda high'},
+    {id:3,name:'Finish other task',priority:'Below kinda high'}
+];
--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-
+import {tasks,task} from '../tasks';
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
   styleUrls: ['./tasks.component.css']
 })
 export class TasksComponent implements OnInit {
-
+  tugas=tasks;
   constructor() { }
 
   ngOnInit() {
