869b5f1491575b52e5ca3ccd8c5a8566236b0e91
Andy Sukanto Kan,Andy A <a.kan@student.fontys.nl>
2018-08-29 20:02:05 +0200

    Created three new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
-import {FormsModule} from '@angular/forms';//"this is where ng model comes from"
+import {FormsModule} from '@angular/forms';
+import { TasksComponent } from './tasks/tasks.component';
+import { DepartmentsComponent } from './departments/departments.component';
+import { EmployeesComponent } from './employees/employees.component';//"this is where ng model comes from"
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent
+    HeroesComponent,
+    TasksComponent,
+    DepartmentsComponent,
+    EmployeesComponent
   ],
   imports: [
     BrowserModule,
