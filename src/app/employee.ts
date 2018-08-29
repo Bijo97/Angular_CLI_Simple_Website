11671ee85a8f050fb2c53debc905350dbb2f4564
Andy Sukanto Kan,Andy A <a.kan@student.fontys.nl>
2018-08-29 20:16:00 +0200

    employee grid added

---
+++ src/app/employee.ts
@@ -0,0 +1,10 @@
+export class employee{
+    id:number;
+    emp_name:string;
+    emp_address:string;
+};
+export const employees:employee[]=[
+    {id:1,emp_name:'biljo',emp_address:'hell'},
+    {id:2,emp_name:'biljo too',emp_address:'deeper hell'},
+    {id:3,emp_name:'hafizh',emp_address:'eindhoven'}
+];
--- src/app/employees/employees.component.ts
+++ src/app/employees/employees.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-
+import{employees,employee} from '../employee';
 @Component({
   selector: 'app-employees',
   templateUrl: './employees.component.html',
   styleUrls: ['./employees.component.css']
 })
 export class EmployeesComponent implements OnInit {
-
+  employeesGroup=employees;
   constructor() { }
 
   ngOnInit() {
