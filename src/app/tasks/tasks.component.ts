af00d069afed4338374453a0461ef78dcc779386
Andy Sukanto Kan,Andy A <a.kan@student.fontys.nl>
2018-09-05 14:21:20 +0200

    Able to create a new entry on tasks

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
-import {tasks,task} from '../tasks';
+import {
+  Component,
+  OnInit
+} from '@angular/core';
+import {
+  tasks,
+  task,
+  priorities
+} from '../tasks';
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
   styleUrls: ['./tasks.component.css']
 })
 export class TasksComponent implements OnInit {
-  tugas=tasks;
-  constructor() { }
+  tugas = tasks;
+  priority = priorities;
+  constructor() {}
+  ngOnInit() {}
 
-  ngOnInit() {
+  newTaskName;
+  newPriority;
+  addTask() {
+    let newId = this.tugas.length + 1;
+    let newTask: task = {
+      id: newId,
+      name: this.newTaskName,
+      priority: this.newPriority
+    };
+    this.tugas.push(newTask);
   }
 
 }
