91c5516e7106124ada1b9d2ee860a80cdc67534a
Billy Jonathan,Billy B <b.jonathan@student.fontys.nl>
2018-09-05 19:43:28 +0700

    Department CREATE Function

--- src/app/departments/departments.component.ts
+++ src/app/departments/departments.component.ts
@@ -6,11 +6,27 @@ import {departments,department} from '../department';
   templateUrl: './departments.component.html',
   styleUrls: ['./departments.component.css']
 })
+
 export class DepartmentsComponent implements OnInit {
   depts=departments;
+  dept_name = null;
+  selectedDept: department;
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  insert(){
+  	let count = this.depts.length + 1;
+  	let dept: department = {
+  		id: count,
+  		dept_name: this.dept_name
+  	};
+  	this.depts.push(dept);
+  }
+
+  show(dept:department):void{
+    this.selectedDept=dept;
+  }
 }
