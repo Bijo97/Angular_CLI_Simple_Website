b8fcd9e236a067e1b84e451b6cf7e5eb15012008
Billy Jonathan,Billy B <b.jonathan@student.fontys.nl>
2018-09-11 03:43:17 +0700

    CRUD for Department

--- src/app/departments/departments.component.ts
+++ src/app/departments/departments.component.ts
@@ -6,11 +6,40 @@ import {departments,department} from '../department';
   templateUrl: './departments.component.html',
   styleUrls: ['./departments.component.css']
 })
+
 export class DepartmentsComponent implements OnInit {
   depts=departments;
+  dept_name = null;
+  selectedDept: department;
+  i: number;
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  insert():void{
+  	let count = this.depts.length + 1;
+  	let dept: department = {
+  		id: count,
+  		dept_name: this.dept_name
+  	};
+  	this.depts.push(dept);
+  }
+
+  show(dept:department, i:number):void{
+    this.selectedDept = dept;
+    this.i = i;
+  }
+
+  update(index:number):void{
+  	this.depts[index].dept_name = this.selectedDept.dept_name;
+  	this.selectedDept = null;
+  }
+
+  delete(index:number):void{
+  	if (index != -1){
+  		this.depts.splice(index, 1);
+  	}
+  }
 }
