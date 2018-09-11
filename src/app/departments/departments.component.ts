d185bc16064bd99070a1fb8a04baffd1dcddb736
Andy Sukanto Kan,Andy A <a.kan@student.fontys.nl>
2018-09-11 13:16:30 +0200

    fixed another merge conflict

--- src/app/departments/departments.component.ts
+++ src/app/departments/departments.component.ts
@@ -1,5 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import {departments,department} from '../department';
+import {
+  Component,
+  OnInit
+} from '@angular/core';
+import {
+  departments,
+  department
+} from '../department';
 
 @Component({
   selector: 'app-departments',
@@ -8,50 +14,37 @@ import {departments,department} from '../department';
 })
 
 export class DepartmentsComponent implements OnInit {
-  depts=departments;
+  depts = departments;
   dept_name = null;
   selectedDept: department;
-<<<<<<< HEAD
   i: number;
-=======
->>>>>>> 91c5516e7106124ada1b9d2ee860a80cdc67534a
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  insert(): void {
+    const count = this.depts.length + 1;
+    const dept: department = {
+      id: count,
+      dept_name: this.dept_name
+    };
+    this.depts.push(dept);
   }
 
-<<<<<<< HEAD
-  insert():void{
-=======
-  insert(){
->>>>>>> 91c5516e7106124ada1b9d2ee860a80cdc67534a
-  	let count = this.depts.length + 1;
-  	let dept: department = {
-  		id: count,
-  		dept_name: this.dept_name
-  	};
-  	this.depts.push(dept);
-  }
-
-<<<<<<< HEAD
-  show(dept:department, i:number):void{
+  show(dept: department, i: number): void {
     this.selectedDept = dept;
     this.i = i;
   }
 
-  update(index:number):void{
-  	this.depts[index].dept_name = this.selectedDept.dept_name;
-  	this.selectedDept = null;
+  update(index: number): void {
+    this.depts[index].dept_name = this.selectedDept.dept_name;
+    this.selectedDept = null;
   }
 
-  delete(index:number):void{
-  	if (index != -1){
-  		this.depts.splice(index, 1);
-  	}
-=======
-  show(dept:department):void{
-    this.selectedDept=dept;
->>>>>>> 91c5516e7106124ada1b9d2ee860a80cdc67534a
+  delete(index: number): void {
+    if (index !== -1) {
+      this.depts.splice(index, 1);
+    }
   }
 }
