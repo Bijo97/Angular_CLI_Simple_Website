6b113a299c4db3648c5aa524260f9531f74fa610
Andy Sukanto Kan,Andy A <a.kan@student.fontys.nl>
2018-09-12 13:14:20 +0200

    Fixed as what tsint suggest

--- src/app/tasks.ts
+++ src/app/tasks.ts
@@ -1,11 +1,22 @@
-export class task{
-    id:number;
-    name:string;
-    priority:string;
+export class task {
+  id: number;
+  name: string;
+  priority: string;
 };
-export const tasks:task[]=[
-    {id:1,name:'Develop Software',priority:'High'},
-    {id:2,name:'Make Treasury Report',priority:'Medium'},
-    {id:3,name:'Operate Machines',priority:'Low'}
+export const tasks: task[] = [{
+    id: 1,
+    name: 'Develop Software',
+    priority: 'High'
+  },
+  {
+    id: 2,
+    name: 'Make Treasury Report',
+    priority: 'Medium'
+  },
+  {
+    id: 3,
+    name: 'Operate Machines',
+    priority: 'Low'
+  }
 ];
-export const priorities:string[]=["Low","Medium","High"];
+export const priorities: string[] = ['Low', 'Medium', 'High'];
