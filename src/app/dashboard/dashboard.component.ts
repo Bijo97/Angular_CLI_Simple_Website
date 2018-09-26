282f9f90a93d4af2741e31a6a167d06cb46cbc96
Andy Sukanto Kan,Andy A <a.kan@student.fontys.nl>
2018-09-26 14:18:50 +0200

    added services to the dashboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit
+} from '@angular/core';
+import {
+  Task,
+} from '../tasks';
+import {
+  Employee
+} from '../employee';
+import {
+  department
+} from '../department';
+import {
+  TasksService
+} from '../tasks.service';
+import {
+  DepartmentsService
+} from '../departments.service';
+import {
+  EmployeeService
+} from '../employee.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,10 +27,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+  tugas: Task[];
+  employeeList: Employee[];
+  departments: department[];
+  constructor(private taskService: TasksService, private empService: EmployeeService, private depService: DepartmentsService) {}
 
-  constructor() { }
+  getTasks(): void {
+    this.taskService.getTasks().subscribe(tugas => this.tugas = tugas);
+  }
+  getEmployees(): void {
+    this.empService.getEmployees().subscribe(employeeList => this.employeeList = employeeList);
+  }
+  getDepartments(): void {
+    this.depService.getDepartments().subscribe(departmentList => this.departments = departmentList);
+  }
 
   ngOnInit() {
+    this.getTasks();
+    this.getEmployees();
+    this.getDepartments();
   }
 
 }
