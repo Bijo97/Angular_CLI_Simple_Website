5e38a19958b262dc76bb1f8d1e25e0fb078053d4
Andy Sukanto Kan,Andy A <a.kan@student.fontys.nl>
2018-10-03 13:49:55 +0200

    Trying to work with typeahead, need figure out how to access object name

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -20,28 +20,27 @@ import {
 import {
   EmployeeService
 } from '../employee.service';
-import {Observable} from 'rxjs';
-import {debounceTime, distinctUntilChanged, map} from 'rxjs/operators';
+import {
+  Observable
+} from 'rxjs';
+import {
+  debounceTime,
+  distinctUntilChanged,
+  map
+} from 'rxjs/operators';
 
-const states = ['Alabama', 'Alaska', 'American Samoa', 'Arizona', 'Arkansas', 'California', 'Colorado',
-  'Connecticut', 'Delaware', 'District Of Columbia', 'Federated States Of Micronesia', 'Florida', 'Georgia',
-  'Guam', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine',
-  'Marshall Islands', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana',
-  'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota',
-  'Northern Mariana Islands', 'Ohio', 'Oklahoma', 'Oregon', 'Palau', 'Pennsylvania', 'Puerto Rico', 'Rhode Island',
-  'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virgin Islands', 'Virginia',
-  'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'];
-  @Component({
-    selector: 'app-tasks',
-    templateUrl: './tasks.component.html',
-    styleUrls: ['./tasks.component.css']
-  })
-  export class TasksComponent implements OnInit {
+@Component({
+  selector: 'app-tasks',
+  templateUrl: './tasks.component.html',
+  styleUrls: ['./tasks.component.css']
+})
+export class TasksComponent implements OnInit {
   // initial variables
   public model: any;
   tugas: Task[];
   employeeList: Employee[];
   departments: department[];
+  departmentsNames: string[];
   newTaskName; // for create
   newDepartment;
   newEmployee;
@@ -88,24 +87,26 @@ const states = ['Alabama', 'Alaska', 'American Samoa', 'Arizona', 'Arkansas', 'C
     } // return tempEmp.eame;
   }
   getEmployeeName(empId: number) {
-   if (empId !== null) {
-    return this.employeeList[empId - 1].emp_name;
-   } else {
-     return 'no employees assigned'; // return tempEmp.eame;
+    if (empId !== null) {
+      return this.employeeList[empId - 1].emp_name;
+    } else {
+      return 'no employees assigned'; // return tempEmp.eame;
+    }
   }
-}
   deleteTask(index: number) {
     this.taskService.deleteTask(index);
   }
   updateTask(index: number, newName: string, newPriority: string) {
     this.taskService.updateTask(index, newName, newPriority);
   }
-  search = (text$: Observable<string>) =>
+  search = (text$: Observable < string > ) =>
     text$.pipe(
       debounceTime(200),
       distinctUntilChanged(),
-      map(term => term.length < 2 ? []
-        : states.filter(v => v.toLowerCase().indexOf(term.toLowerCase()) > -1).slice(0, 10))
+      map(term => term.length < 2 ? [] :
+        this.tugas.filter(v => v.name.toLowerCase().indexOf(term.toLowerCase()) > -1).slice(0, 10))
     )
+    formatter = (x: {name: string}) => x.name;
+
 
 }
